7c9d9d0031b3e6c1ed8dd7aab2db5493b3802e57
feno1973 <79587629+feno1973@users.noreply.github.com>
2026-06-17 12:01:26 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+g.clear();
+
+g.setFont("Vector", 20);
+g.setFontAlign(0, 0);
+
+g.drawString("FENO", 88, 45);
+g.drawString("SYSCASIO", 88, 75);
+
+g.setFont("6x8", 2);
+g.drawString("v0.01", 88, 115);
+g.drawString("Booting...", 88, 140);
